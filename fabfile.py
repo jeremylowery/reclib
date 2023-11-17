c5dcd6ee04a604120b7fa640854c9cb409cfd52f
Jeremy Lowery <jeremy@ehorx.com>
2023-11-17 15:24:50 -0600

    simplified fabfile.

--- fabfile.py
+++ fabfile.py
@@ -1,100 +1,15 @@
-import commands
-import glob
-import os
-import re
-import shutil
-
 from fabric.api import *
 
-#env.hosts = [']
 os.chdir(os.path.dirname(__file__))
 
-def _get_version():
-    return commands.getoutput(r'cat setup.py  | grep version | cut -d\" -f2')
+version = "0.2.0"
+wheel = "reclib-{0}-py3-none-any.whl".format(version)
 
-def update():
-    local("python setup.py --quiet install")
+def build():
+    local("python3 -m build")
 
-def update3():
-    local("python3 setup.py --quiet install")
+def twine():
+    local("twine upload dist/{0}".format(wheel))
 
-def deploy(revision='tip'):
-    """ Send off to production server and install """
-
-    # Clean out working directory
-    if os.path.exists("/tmp/reclib"):
-        local("rm -rf /tmp/reclib")
-
-    # Pull source code from remote repository
-    # Pull out a clean version of the code to build from
-    local("hg clone -qr %s . /tmp/reclib" % revision)
-
-    # Build a source distribution
-    with lcd("/tmp/reclib"):
-        local("python setup.py -q sdist")
-
-    # distutils creates the tarball with the version from setup.py in it. We
-    # do not know that version here, so we have to parse it out of the tar ball
-    # file name.
-    flist = glob.glob('/tmp/reclib/dist/*.tar.gz')
-    if len(flist) != 1:
-        print 'Expected tarball in /tmp/reclib/dist. found %s' % ' '.join(flist)
-        return
-
-    tb_path = flist[0]
-    tb_fname = os.path.basename(tb_path)
-    # the app_dir is the directory that the tarball is going to place the files
-    # in. This is the app name - version.
-    app_dir = tb_fname[:tb_fname.find('.tar.gz')]
-
-    # Send the source distribution to host
-    put(tb_path, "/tmp")
-
-    # untar the source distribution
-    with cd("/tmp"):
-        run("tar xzf %s" % tb_fname)
-
-    # Install the source package on the host
-    with cd("/tmp/%s" % app_dir):
-        run("python setup.py -q install --prefix=/usr/local")
-
-
-def deploy3(revision='tip'):
-    """ Send off to production server and install """
-
-    # Clean out working directory
-    if os.path.exists("/tmp/reclib"):
-        local("rm -rf /tmp/reclib")
-
-    # Pull source code from remote repository
-    # Pull out a clean version of the code to build from
-    local("hg clone -qr %s . /tmp/reclib" % revision)
-
-    # Build a source distribution
-    with lcd("/tmp/reclib"):
-        local("python3 setup.py -q sdist")
-
-    # distutils creates the tarball with the version from setup.py in it. We
-    # do not know that version here, so we have to parse it out of the tar ball
-    # file name.
-    flist = glob.glob('/tmp/reclib/dist/*.tar.gz')
-    if len(flist) != 1:
-        print 'Expected tarball in /tmp/reclib/dist. found %s' % ' '.join(flist)
-        return
-
-    tb_path = flist[0]
-    tb_fname = os.path.basename(tb_path)
-    # the app_dir is the directory that the tarball is going to place the files
-    # in. This is the app name - version.
-    app_dir = tb_fname[:tb_fname.find('.tar.gz')]
-
-    # Send the source distribution to host
-    put(tb_path, "/tmp")
-
-    # untar the source distribution
-    with cd("/tmp"):
-        run("tar xzf %s" % tb_fname)
-
-    # Install the source package on the host
-    with cd("/tmp/%s" % app_dir):
-        run("python3 setup.py -q install --prefix=/usr/local")
+def develop():
+    local("python3 -m pip -e .")
