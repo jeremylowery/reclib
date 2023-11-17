38e77d4915e49ba3e83b11f792809af80054199c
Jeremy Lowery <jeremy@ehorx.com>
2023-11-17 15:29:37 -0600

    added tarball to twine

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,7 @@ from fabric.api import *
 
 version = "0.2.2"
 wheel = "reclib-{0}-py3-none-any.whl".format(version)
+tarball = "reclib-{0}.tar.gz".format(version)
 
 def build():
     local("python3 -m build")
@@ -10,7 +11,7 @@ def bumpver():
     local("bumpver update -p --commit")
 
 def twine():
-    local("twine upload dist/{0}".format(wheel))
+    local("twine upload dist/{0} dist/{1}".format(wheel, tarball))
 
 def develop():
     local("python3 -m pip -e .")
