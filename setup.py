23059e45b9a03f63d9b1cb28bb261e25b568e8a8
Jeremy Lowery <jeremy@bitrel.com>
2023-11-17 14:58:06 -0600

    moved to setuptools

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env python
-import os
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="reclib",
-    version="2011.9.30",
+    version="2023.11.17",
     provides=["reclib"],
     description="reclib library",
     long_description="record parsing and validating library",
     license="",
+    install_requires=[
+        "future"
+    ],
     author="Jeremy Lowery",
     author_email="jeremy@bitrel.com",
     url="http://bitrel.com",
