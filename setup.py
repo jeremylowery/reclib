3b578d47085221316d251b92f158228e8db87ddd
Jeremy Lowery <jeremy@ehorx.com>
2024-10-04 11:34:24 -0500

    made future python2 dependency only

--- setup.py
+++ setup.py
@@ -7,12 +7,10 @@ setup(
     description="reclib library",
     long_description="record parsing and validating library",
     license="",
-    install_requires=[
-        "future"
-    ],
+    install_requires=["future; python_version == '2.7'"],
     author="Jeremy Lowery",
     author_email="jeremy@bitrel.com",
     url="http://bitrel.com",
     platforms="POSIX",
-    packages=['reclib', 'reclib.parse', 'reclib.format']
+    packages=["reclib", "reclib.parse", "reclib.format"],
 )
